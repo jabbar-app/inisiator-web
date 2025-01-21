431025874dde277549aa5ee42ec999cbe9f5d325
Jab <jabbarpanggabean@gmail.com>
2025-01-21 15:12:47 +0700

    Add Meta Description.

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -8,7 +8,7 @@
   <meta name="viewport"
     content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-  <title>Register | Inisiator</title>
+  <title>{{ Route::is('login') ? 'Login' : 'Register' }} | Inisiator</title>
 
   <meta name="description" content="" />
 
